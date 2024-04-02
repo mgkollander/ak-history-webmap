1705d5199673cce3a7f1ebee2b6c2af5b266e798
Mary Kollander <124931791+mgkollander@users.noreply.github.com>
2024-04-02 04:22:31 -0800

    Organization
    - Organized file and folder names
    - Began PruneCluster marker handling switch (commented out for now)

--- map.js
+++ map.js
@@ -95,3 +95,87 @@ window.addEventListener('resize', (() => {
         }, 250);
     };
 })());
+
+/* ------- Switching to PruneCluster WIP ------- */
+
+/*
+const calculateMinZoom = () => document.documentElement.clientWidth < 768 ? 4 : 5;
+
+const initializeMap = () => {
+    const map = L.map('map', {
+        maxBounds: [[46.56, -189.14],[73.15, -123.93]],
+        maxBoundsViscosity: 0.5,
+        minZoom: calculateMinZoom(),
+        maxZoom: 8,
+        attributionControl: false
+    }).setView([64.793, -153.040], calculateMinZoom());
+    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map);
+    return map;
+};
+
+const initializeSlider = () => {
+    const slider = document.getElementById('slider');
+    if (slider.noUiSlider) slider.noUiSlider.destroy();
+    noUiSlider.create(slider, {
+        start: [1750, 2020],
+        connect: true,
+        tooltips: [true, true],
+        format: { to: value => Math.round(value), from: value => value },
+        range: { 'min': 1750, 'max': 2020 }
+    });
+    mergeTooltips(slider, Math.floor(8600 / window.innerWidth), ' - ');
+    slider.noUiSlider.on('update', values => {
+        currentStartYear = parseInt(values[0]);
+        currentEndYear = parseInt(values[1]);
+    });
+};
+
+const map = initializeMap();
+
+let [currentStartYear, currentEndYear] = [1750, 2020];
+initializeSlider();
+
+window.addEventListener('resize', (() => {
+    let timeoutId;
+    return () => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            map.setMinZoom(calculateMinZoom());
+            initializeSlider();
+        }, 250);
+    };
+})());
+
+const loadGeoJSON = (map) => {
+    var leafletView = new PruneClusterForLeaflet();
+    leafletView.Cluster.Size = 160;
+    leafletView.PrepareLeafletMarker = function(leafletMarker, data) {
+        var endDate = data.properties.endDate;
+        var popupContent = "<b>" + data.properties.description + "</b><br>End Date: " + endDate;
+        const customIcon = L.icon({
+            iconUrl: 'data/icons/pin.png',
+            iconSize: [50, 50],
+            iconAnchor: [3, 30],
+            popupAnchor: [22, -28],
+        });
+        leafletMarker.setIcon(customIcon);
+        leafletMarker.bindPopup(popupContent);
+    };
+    var pruneClusterLayer = L.layerGroup();
+
+    fetch('markerData.geojson')
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                var marker = new PruneCluster.Marker(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
+                marker.data.properties = feature.properties;
+                leafletView.RegisterMarker(marker);
+            });
+            //filterMarkers();
+            pruneClusterLayer.addLayer(leafletView);
+            map.addLayer(pruneClusterLayer);
+    });
+}
+
+const geojsonLayer = loadGeoJSON(map);
+*/

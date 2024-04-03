1eabf8ec5819a84424abade629f8c5eb084684c7
Mary Kollander <124931791+mgkollander@users.noreply.github.com>
2024-04-02 21:36:16 -0800

    PruneCluster implemented
    - Implemented PruneCluster
    - Timeline slider functionality removed for now

--- map.js
+++ map.js
@@ -1,19 +1,5 @@
-// Function to calculate minimum zoom based on screen width
+// Function to calculate minimimum zoom based on current screen width
 const calculateMinZoom = () => document.documentElement.clientWidth < 768 ? 4 : 5;
-
-// Function to filter markers based on current slider years
-const filterMarkers = () => {
-    geojsonLayer.eachLayer(layer => {
-        const { startDate, endDate } = layer.feature.properties;
-        const inRange = startDate <= currentEndYear && endDate >= currentStartYear;
-
-        if (inRange && !map.hasLayer(layer)) {
-            map.addLayer(layer);
-        } else if (!inRange && map.hasLayer(layer)) {
-            map.removeLayer(layer);
-        }
-    });
-};
 
 // Function to initialize the slider
 const initializeSlider = () => {
@@ -31,151 +17,64 @@ const initializeSlider = () => {
     });
 
     mergeTooltips(slider, Math.floor(8600 / window.innerWidth), ' - ');
-
-    // Listen for slider changes
-    slider.noUiSlider.on('update', values => {
-        currentStartYear = parseInt(values[0]);
-        currentEndYear = parseInt(values[1]);
-        filterMarkers();
-    });
+    return slider;
 };
 
-// Function to initialize map
-const initializeMap = () => {
-    const map = L.map('map', {
-        maxBounds: [[46.56, -189.14],[73.15, -123.93]],
-        maxBoundsViscosity: 0.5,
-        minZoom: calculateMinZoom(),
-        maxZoom: 8,
-        attributionControl: false
-    }).setView([64.793, -153.040], calculateMinZoom());
-
-    // Add tile layer to map
-    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map);
-
-    return map;
-};
-
-// Function to load GeoJSON data
+// Function to load GeoJSON marker data
 const loadGeoJSON = (map) => {
-    const geojsonLayer = L.geoJSON().addTo(map);
-    const customIcon = L.icon({
-        iconUrl: 'data/icons/pin.png',
-        iconSize: [50, 50],
-        iconAnchor: [3, 30],
-        popupAnchor: [22, -28],
-    });
-    geojsonLayer.on('layeradd', (event) => {
-        const layer = event.layer;
-        layer.setIcon(customIcon);
-    });
-    fetch('markerData.geojson')
-        .then(response => response.json())
-        .then(data => {
-            geojsonLayer.addData(data);
-            filterMarkers();
-        });
-    return geojsonLayer;
-};
-
-const map = initializeMap();
-const geojsonLayer = loadGeoJSON(map);
-
-let [currentStartYear, currentEndYear] = [1750, 2020];
-initializeSlider();
-
-// Listen for window resize event (with debouncing)
-window.addEventListener('resize', (() => {
-    let timeoutId;
-    return () => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            map.setMinZoom(calculateMinZoom());
-            initializeSlider();
-        }, 250);
-    };
-})());
-
-/* ------- Switching to PruneCluster WIP ------- */
-
-/*
-const calculateMinZoom = () => document.documentElement.clientWidth < 768 ? 4 : 5;
-
-const initializeMap = () => {
-    const map = L.map('map', {
-        maxBounds: [[46.56, -189.14],[73.15, -123.93]],
-        maxBoundsViscosity: 0.5,
-        minZoom: calculateMinZoom(),
-        maxZoom: 8,
-        attributionControl: false
-    }).setView([64.793, -153.040], calculateMinZoom());
-    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map);
-    return map;
-};
-
-const initializeSlider = () => {
-    const slider = document.getElementById('slider');
-    if (slider.noUiSlider) slider.noUiSlider.destroy();
-    noUiSlider.create(slider, {
-        start: [1750, 2020],
-        connect: true,
-        tooltips: [true, true],
-        format: { to: value => Math.round(value), from: value => value },
-        range: { 'min': 1750, 'max': 2020 }
-    });
-    mergeTooltips(slider, Math.floor(8600 / window.innerWidth), ' - ');
-    slider.noUiSlider.on('update', values => {
-        currentStartYear = parseInt(values[0]);
-        currentEndYear = parseInt(values[1]);
-    });
-};
-
-const map = initializeMap();
-
-let [currentStartYear, currentEndYear] = [1750, 2020];
-initializeSlider();
-
-window.addEventListener('resize', (() => {
-    let timeoutId;
-    return () => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            map.setMinZoom(calculateMinZoom());
-            initializeSlider();
-        }, 250);
-    };
-})());
-
-const loadGeoJSON = (map) => {
-    var leafletView = new PruneClusterForLeaflet();
+    let leafletView = new PruneClusterForLeaflet();
     leafletView.Cluster.Size = 160;
+
     leafletView.PrepareLeafletMarker = function(leafletMarker, data) {
-        var endDate = data.properties.endDate;
-        var popupContent = "<b>" + data.properties.description + "</b><br>End Date: " + endDate;
+        let popupContent = "<b>" + data.properties.startDate + ' - ' + data.properties.endDate + "</b><br>" + data.properties.description;
         const customIcon = L.icon({
             iconUrl: 'data/icons/pin.png',
             iconSize: [50, 50],
             iconAnchor: [3, 30],
-            popupAnchor: [22, -28],
+            popupAnchor: [22, -28]
         });
         leafletMarker.setIcon(customIcon);
         leafletMarker.bindPopup(popupContent);
     };
-    var pruneClusterLayer = L.layerGroup();
+    let pruneClusterLayer = L.layerGroup();
 
     fetch('markerData.geojson')
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
-                var marker = new PruneCluster.Marker(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
+                let marker = new PruneCluster.Marker(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
                 marker.data.properties = feature.properties;
                 leafletView.RegisterMarker(marker);
             });
-            //filterMarkers();
             pruneClusterLayer.addLayer(leafletView);
             map.addLayer(pruneClusterLayer);
     });
 }
 
+const map = L.map('map', {
+    maxBounds:[[46.56, -189.14],[73.15, -123.93]],
+    maxBoundsViscosity: 0.5,
+    minZoom: calculateMinZoom(),
+    maxZoom: 8,
+    attributionControl: false
+}).setView([64.793, -153.040], calculateMinZoom());
+
+// Add tile layer
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map);
+
 const geojsonLayer = loadGeoJSON(map);
-*/
+
+let [currentStartYear, currentEndYear] = [1750, 2020];
+initializeSlider();
+
+// Listen for changes in screen size (with debouncing)
+window.addEventListener('resize', (() => {
+    let timeoutId;
+    return () => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            map.setMinZoom(calculateMinZoom());
+            initializeSlider();
+        }, 250);
+    };
+})());

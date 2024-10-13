c619a93208a2dabf53131b76455577641f25d325
rjhollowood <robby@tresdrol.com>
2024-10-13 12:44:17 -0800

    Adding credit page

--- src/constants.js
+++ src/constants.js
@@ -10,10 +10,11 @@ export const MAX_BOUNDS = [[44.47, -205.84], [74.64, -116.37]];
 export const INITIAL_COORDINATES = [64.793, -153.040];
 export const MAX_BOUNDS_VISCOSITY = 0.5;
 export const MAX_ZOOM = 8;
-export const OTM_TILE_LAYER = ['https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | © <a href="https://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`];
-export const OSM_TILE_LAYER = ['https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>`];
-export const ST_TILE_LAYER = ['https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png', `<a href="<a href="https://stamen.com/" target="_blank">Stamen Design</a> | <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> | <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> | <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>`];
-export const STB_TILE_LAYER = ['https://tiles.stadiamaps.com/tiles/stamen_terrain_background/{z}/{x}/{y}{r}.png', `<a href="<a href="https://stamen.com/" target="_blank">Stamen Design</a> | <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> | <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> | <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>`];
+const RESEARCH_CREDIT = ' | Made by <a href="credits.html" target="_blank">Dr. Maria Williams and co.</a>';
+export const OTM_TILE_LAYER = ['https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | © <a href="https://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)` + RESEARCH_CREDIT];
+export const OSM_TILE_LAYER = ['https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>`+ RESEARCH_CREDIT];
+export const ST_TILE_LAYER = ['https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png', `<a href="<a href="https://stamen.com/" target="_blank">Stamen Design</a> | <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> | <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> | <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>` + RESEARCH_CREDIT];
+export const STB_TILE_LAYER = ['https://tiles.stadiamaps.com/tiles/stamen_terrain_background/{z}/{x}/{y}{r}.png', `<a href="<a href="https://stamen.com/" target="_blank">Stamen Design</a> | <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> | <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> | <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>`+ RESEARCH_CREDIT];
 
 
 /* ---------- MARKER HANDLING ---------- */
